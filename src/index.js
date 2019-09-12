56f84974d568d41d505d454b06d4a5e4a1760c29
Roman Belik <roman.belik@chisw.com>
2019-09-12 16:15:24 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+    name: 'Paul',
+    secondName: 'Petrov'
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function reducer(state=initialState, action) {
+    switch (action.type) {
+        case 'CHANGE_NAME':
+            return { ...state, name: action.payload }
+        case 'CHANGE_SECOND_NAME':
+            return { ...state, secondName: action.payload }
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer);
+
+console.log('1-----', store.getState());
+
+const changeName = {
+    type: 'CHANGE_NAME',
+    payload: 'Ivan'
+}
+
+const changeSecondName = {
+    type: 'CHANGE_SECOND_NAME',
+    payload: 'Sidorov'
+}
+
+store.dispatch(changeName);
+
+console.log('2-----', store.getState());
+
+store.dispatch(changeSecondName);
+
+console.log('3-----', store.getState());
